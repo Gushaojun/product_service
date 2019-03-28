e1460180e9095b82a44772a5863d1cb3af666b48
46160 <461609227@qq.com>
2019-03-28 18:07:53 +0800

    升级v2

--- src/main/java/net/xdclass/product_service/controller/ProductController.java
+++ src/main/java/net/xdclass/product_service/controller/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.TimeUnit;
 
 @RestController
-@RequestMapping("/api/v1/product")
+@RequestMapping("/api/v2/product")
 public class ProductController {
 
 
